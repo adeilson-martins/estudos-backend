2fbe1c154b50d0756b83b23acb7c518ff3ce8c7f
Adeilson Martins <adeilsonmartinslima@hotmail.com>
2026-09-22 19:46:48 -0300

    Pacotes em Go

---
+++ golang-do-zero/pacotes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	Adeilson "strings"
+)
+
+func main() {
+	fmt.Println("Adeilson")
+	fmt.Println(Adeilson.Split("Martins", ""))
+}
+
+/* sem alterar o nome do pacote,
+usaríamos chamando strings.Func()
+strings.Slipt() */
