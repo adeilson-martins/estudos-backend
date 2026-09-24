03677d45ead4726f3c467a22426d416d677dc7b0
Adeilson Martins <adeilsonmartinslima@hotmail.com>
2026-09-24 18:37:19 -0300

    Tipos de Dados

---
+++ golang-do-zero/tipos.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// bool - (verdadeiro ou falso)
+	fmt.Printf("Type: %T - Value: %v\n", true, false)
+
+	// string - (sequência de bytes)
+	fmt.Printf("Type: %T - Value: %v\n", "Adeilson", "Adeilson")
+	fmt.Printf("Type: %T - Value: %v\n", "11", "11")
+
+	// int - (inteiro)
+	fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+
+	// float - (float64/float32 - decimal)
+	fmt.Printf("Type: %T - Value: %v\n", 1.28, 1.28)
+}
