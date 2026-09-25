619dbf2117b839e792d2a57897db5b7590792ee6
Adeilson Martins <adeilsonmartinslima@hotmail.com>
2026-09-25 17:58:38 -0300

    Valores Zero

---
+++ golang-do-zero/zerovalues.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variáveis Valores Zero
+	var a int     // int - (inteiro)
+	var b float64 // float - (float64/float32 - decimal)
+	var c bool    // bool - (verdadeiro ou falso)
+	var d string  // string - (sequência de bytes)
+
+	//Exibindo as Variáveis
+	fmt.Printf("int: %v\n", a)
+	fmt.Printf("float: %v\n", b)
+	fmt.Printf("bool: %v\n", c)
+	fmt.Printf("string: %q\n", d)
+}
